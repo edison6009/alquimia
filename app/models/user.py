ed30c75c740d932f44e0cfdf7d845bb6e6c90d4e
edison6009 <edison09niversidad@gmail.com>
2025-05-31 21:59:06 -0400

    integracion de base en el init de la carpeta models

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,9 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from app.models import Base
 
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
+    name = Column(String(255), nullable=False)
+    email = Column(String(255), unique=False, nullable=True)
--- main.py
+++ main.py
@@ -5,6 +5,15 @@ BASE_DIR = Path(__file__).resolve().parent
 sys.path.append(str(BASE_DIR))
 
 from settings import *
+from app.controllers.user_controller import get_users, add_user
 
 if __name__ == "__main__":
-    print(engine)
+    add_user("edison")
+
+    # Obtener todos los usuarios registrados
+    usuarios = get_users()
+
+    # Mostrar los resultados
+    print("Usuarios en la base de datos:")
+    for usuario in usuarios:
+        print(f"- {usuario.name}")
